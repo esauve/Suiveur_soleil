8622c7234a4c76b0a9b90bedf57680e2ae7fd331
esauve <sauvee@hotmail.com>
2021-06-02 10:24:55 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,22 @@
-let intensité_max_2 = 0
+let intensité = 0
 let angle_max = 0
-let angle_max_2 = 0
 let intensité_max = 0
-let angle = 180
-pins.servoWritePin(AnalogPin.P0, 0)
+let angle = 0
+pins.servoWritePin(AnalogPin.P1, 0)
+serial.redirectToUSB()
 basic.forever(function () {
     intensité_max = 0
-    angle_max_2 = 0
+    angle_max = 0
     for (let angle = 0; angle <= 180; angle++) {
-        pins.servoWritePin(AnalogPin.P0, angle)
-        if (input.lightLevel() > intensité_max) {
+        pins.servoWritePin(AnalogPin.P1, angle)
+        basic.pause(100)
+        intensité = input.lightLevel()
+        serial.writeLine("" + angle + " , " + intensité + " , " + intensité_max)
+        if (intensité > intensité_max) {
             angle_max = angle
-            intensité_max = input.lightLevel()
+            intensité_max = intensité
         }
-        basic.pause(10)
     }
-    for (let angle = 0; angle <= 180; angle++) {
-        pins.servoWritePin(AnalogPin.P0, 180 - angle)
-        if (input.lightLevel() > intensité_max_2) {
-            intensité_max_2 = angle
-            intensité_max_2 = input.lightLevel()
-        }
-        basic.pause(10)
-    }
-    angle = (angle_max + (180 - angle_max_2)) / 2
-    pins.servoWritePin(AnalogPin.P0, angle)
+    pins.servoWritePin(AnalogPin.P1, angle_max)
     basic.pause(5000)
 })
